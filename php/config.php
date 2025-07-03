8665dc0ffbb3ee0a4b31911b500f71fef1e17383
Sandun Navodya <sandunnavodya2002@gmail.com>
2025-07-04 01:28:30 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-    $host = '127.0.0.1:3307';
-=======
     $host = 'localhost';
     $port = 3307;
->>>>>>> origin
     $dbname = 'taxi_meeya';
     $username = "root";
     $password = "";
@@ -12,19 +8,8 @@
     $conn = mysqli_connect($host, $username, $password, $dbname, $port);
 
     if (!$conn) {
-<<<<<<< HEAD
-        error_log("Database connection failed: " . mysqli_connect_error());
-        die("Connection failed. Please try again later.");
-
-    }
-
-    else {
-        // Uncomment the line below for debugging purposes
-        echo "Database connection successful.";
-=======
         die("Connection failed. Please try again later.".mysqli_connect_error());
     } else {
         // echo "Database connection successful.";
->>>>>>> origin
     }
 ?>
